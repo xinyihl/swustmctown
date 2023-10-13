bdb3b296be4cf8d9f453f6d7d6ce2303b2d31f77
xinyihl <1012737146@qq.com>
2023-10-13 22:30:47 +0800

    Initial commit

---
+++ src/main/java/org/swustmc/Swustmctown.java
@@ -0,0 +1,17 @@
+package org.swustmc;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class Swustmctown extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
