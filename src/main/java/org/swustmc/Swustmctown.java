44016c0c1e2f06a7dc966882b9ea90914b371fdb
xinyihl <1012737146@qq.com>
2023-10-13 22:38:05 +0800

    todo

--- src/main/java/org/swustmc/Swustmctown.java
+++ src/main/java/org/swustmc/Swustmctown.java
@@ -14,4 +14,11 @@ public final class Swustmctown extends JavaPlugin {
     public void onDisable() {
         // Plugin shutdown logic
     }
+
+    /*todo 玩家小镇
+      todo 镇长支持设置小镇的bluemap的标记、小镇称号、小镇镇名
+      todo PlaceholderAPI变量 （小镇称号/小镇镇名）
+      todo 支持玩家添加全服bluemap标记
+     */
+
 }
